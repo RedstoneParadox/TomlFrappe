42148e0557f6de385164ee2341b0a94743223ee5
RedstoneParadox <spinjitzu00@gmail.com>
2024-12-28 13:43:27 -0800

    Parser

---
+++ src/main/java/io/github/redstoneparadox/tomlfrappe/parser/Parser.java
@@ -0,0 +1,13 @@
+package io.github.redstoneparadox.tomlfrappe.parser;
+
+import io.github.redstoneparadox.tomlfrappe.document.TomlElement;
+import io.github.redstoneparadox.tomlfrappe.document.TomlTable;
+
+import java.util.List;
+
+public class Parser {
+	public TomlTable parse(List<Token> tokens) {
+
+		return null;
+	}
+}
